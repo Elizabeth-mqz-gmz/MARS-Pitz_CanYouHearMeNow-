e91b1e908700c0b60b194b389930c4557c6bee8c
Salvador04 <salglzj@lcg.unam.mx>
2020-10-03 15:27:45 -0500

    feat: we created the one-touch algorithm

--- antenna.py
+++ antenna.py
@@ -3,32 +3,25 @@ pygame.init()
 
 #Set window dimensions
 displayWidth = 800
-displayWeight = 800
+displayHeight = 800
 
 #Initiate the window
 gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
 pygame.display.set_caption('MARS-Pitz - Antenna')
-#Setting clock for game's time
-clock = pygame.time.Clock()
-
-antennaImg = pygame.image.load('antenna.png')
-
-time = 0
 
 
-
-
-
+# antennaImg = pygame.image.load('antenna.png')
+bandera = False
 
 def AntennaDisplay(x,y):
     gameDisplay.blit(antennaImg, (x,y))
 
 
 # Starts the game
-def GameLoop():
+def GameLoop(bandera):
 
     x = (displayWidth * 0.7)
-    y = (display_Height * 0.8)
+    y = (displayHeight * 0.8)
 
     # Loop if the user doesn't close the game
     gameExit = False
@@ -44,36 +37,38 @@ def GameLoop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
 
-                    if (pygame.time.get_ticks):
-                        time = pygame.time.get_ticks
+                    if bandera:
+                        clock.tick()
+                        print ("Se reinicio el tiempo")
 
 
-
-
-                    if (pygame.time.Clock.get_time()/1000) == 4:
-                        cool
-                    tonto
-                else:
-                    print('tonto')
+                    else:
+                        #Setting clock for game's time
+                        print ("El origen")
+                        clock = pygame.time.Clock()
+                        clock.tick()
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
-                    time = pygame.time.Clock.tick
+                    #time = pygame.time.Clock.tick
 
+                    if bandera:
+                        time = clock.get_time()/1000
 
-                    if time = 0:
-                        start tick()
+                        if time > 3:
+                            #bandera = False
+                            print ("Mal hecho")
+
+                        else:
+                            #x_change = 5
+                            print ("Bien hecho")
+                            clock.tick()
 
                     else:
-                        if get_time < 4:
-                            start tick()
-                            se mueve la flecha
-                        else:
-                            pierdes
+                        bandera = True
 
-
-
-
+# Starts
+GameLoop(bandera)
 
 # Exit from the game window
 game.quit()
