b816bf9b1c30fc4bf706f2522602b2b83b812ecc
Adrian Rodriguez <adrian.rodriguez7109@gmail.com>
2020-10-04 16:34:49 -0500

    Basic map for minigames

---
+++ Code/settings.py
@@ -0,0 +1,27 @@
+import pygame as pg
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 600
+HEIGHT = 600
+FPS = 60
+TITLE = "Mars-Pitz"
+BGCOLOR = LIGHTGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
+
+# Player settings
+PLAYER_SPEED = 300
+PLAYER_IMG = 'manBlue_hold.png'
+PLAYER_ROTATION_SPEED = 250
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
---
+++ Code/sprites.py
@@ -0,0 +1,154 @@
+import pygame as pg
+from settings import *
+from tilemap import collide_hit_rect
+import pytmx
+
+from Minigames.antena import Antena
+
+vec = pg.math.Vector2
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = game.player_img
+        self.rect = self.image.get_rect()
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
+        self.vel = vec(0, 0)
+        self.pos = vec(x, y) * TILESIZE
+        self.rot = 0
+
+    def get_keys(self):
+        self.rot_speed = 0
+        self.vel = vec(0, 0)
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.rot = 180
+            self.vel.x = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.rot = 0
+            self.vel.x = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.rot = 90
+            self.vel.y = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.rot = 270
+            self.vel.y = PLAYER_SPEED
+        if keys[pg.K_f]:
+            if self.collide_with_mini_game() and self.game.go_to_minigame and not self.game.on_minigame:
+                print(self.game.go_to_minigame)
+                self.game.on_minigame = self.game.go_to_minigame
+                self.game.minigame_instance = Antena(self.quit, self.game)
+
+    def quit(self):
+        self.game.go_to_minigame = False
+        self.game.on_minigame = False
+        self.game.minigame_instance = False
+
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
+            if hits:
+                if self.vel.x > 0:
+                    self.pos.x = hits[0].rect.left - self.hit_rect.width / 2.0
+                if self.vel.x < 0:
+                    self.pos.x = hits[0].rect.right + self.hit_rect.width / 2.0
+                self.vel.x = 0
+                self.hit_rect.centerx = self.pos.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False, collide_hit_rect)
+            if hits:
+                if self.vel.y > 0:
+                    self.pos.y = hits[0].rect.top - self.hit_rect.height / 2.0
+                if self.vel.y < 0:
+                    self.pos.y = hits[0].rect.bottom + self.hit_rect.height / 2.0
+                self.vel.y = 0
+                self.hit_rect.centery = self.pos.y
+
+    def collide_with_mini_game(self):
+        hits = pg.sprite.spritecollide(self, self.game.mini_game, False, collide_hit_rect)
+        return hits
+
+
+    def update(self):
+        self.get_keys()
+        self.rot = (self.rot + self.rot_speed * self.game.dt) % 360
+        self.image = pg.transform.rotate(self.game.player_img, self.rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+        self.pos += self.vel * self.game.dt
+        self.hit_rect.centerx = self.pos.x
+        self.collide_with_walls('x')
+        self.hit_rect.centery = self.pos.y
+        self.collide_with_walls('y')
+        self.rect.center = self.hit_rect.center
+        if self.collide_with_mini_game():
+            self.game.go_to_minigame = self.collide_with_mini_game()[0].name
+        else:
+            self.game.go_to_minigame = False
+
+class TiledMap:
+    def __init__(self, filename):
+        tm = pytmx.load_pygame(filename, pixelalpha=True)
+        self.width = tm.width * tm.tilewidth
+        self.height = tm.height * tm.tileheight
+        self.tmxdata = tm
+
+    def render(self, surface):
+        ti = self.tmxdata.get_tile_image_by_gid
+        for layer in self.tmxdata.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = ti(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))
+
+    def make_map(self):
+        temp_surface = pg.Surface((self.width, self.height))
+        self.render(temp_surface)
+        return temp_surface
+
+
+class Obstacle(pg.sprite.Sprite):
+    def __init__(self, game, x, y, w, h):
+        self.groups = game.walls
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = pg.Rect(x, y, w, h)
+        self.x = x
+        self.y = y
+        self.rect.x = x
+        self.rect.y = y
+
+
+class MiniGame(pg.sprite.Sprite):
+    def __init__(self, game, name, x, y, w, h):
+        self.groups = game.mini_game
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(GREEN)
+        self.rect = pg.Rect(x, y, w, h)
+        self.x = x
+        self.y = y
+        self.rect.x = x
+        self.rect.y = y
+        self.name = name
+
+# class Wall(pg.sprite.Sprite):
+#     def __init__(self, game, x, y):
+#         self.groups = game.all_sprites, game.walls
+#         pg.sprite.Sprite.__init__(self, self.groups)
+#         self.game = game
+#         self.image = pg.Surface((TILESIZE, TILESIZE))
+#         self.image.fill(GREEN)
+#         self.rect = self.image.get_rect()
+#         self.x = x
+#         self.y = y
+#         self.rect.x = x * TILESIZE
+#         self.rect.y = y * TILESIZE
